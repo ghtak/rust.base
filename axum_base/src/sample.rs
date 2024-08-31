06c194324eb1702edae4d55599b3a696527eff51
ghtak <gwonhyeong.tak@gmail.com>
2024-08-31 21:21:16 +0900

    cookie

--- axum_base/src/sample.rs
+++ axum_base/src/sample.rs
@@ -1,24 +1,31 @@
 use axum::{
-    response::IntoResponse,
+    http::header::SET_COOKIE,
+    response::{AppendHeaders, IntoResponse},
     routing::{get, post},
     Router,
 };
+use axum_extra::extract::{cookie::Cookie, CookieJar};
 use serde::{Deserialize, Serialize};
 use utoipa::{OpenApi, ToSchema};
 
-use crate::{
-    app_state::AppState,
-    basic::extract::{Json, Path},
+use crate::basic::{
+    error::Error,
+    extract::{Json, Path},
 };
 
 #[derive(OpenApi)]
-#[openapi(paths(sample_post, sample_path), components(schemas(Sample)))]
+#[openapi(
+    paths(sample_post, sample_path, get_cookie, set_cookie),
+    components(schemas(Sample))
+)]
 pub(super) struct Api;
 
 pub fn router() -> Router {
     Router::new()
         .route("/sample/", post(sample_post))
         .route("/sample/:user_id/teams/:team_id", get(sample_path))
+        .route("/sample/set_cookie", get(set_cookie))
+        .route("/sample/get_cookie", get(get_cookie))
 }
 
 #[derive(Debug, Serialize, Deserialize, ToSchema)]
@@ -52,4 +59,16 @@ struct SampleParams {
 )]
 async fn sample_path(Path(sp): Path<SampleParams>) -> impl IntoResponse {
     axum::Json(sp)
+}
+
+#[utoipa::path(get, path = "/sample/set_cookie")]
+async fn set_cookie(jar: CookieJar) -> impl IntoResponse {
+    let set_cookie = jar.add(Cookie::new("foo", "bar"));
+    (set_cookie, "SetCookie")
+}
+
+#[utoipa::path(get, path = "/sample/get_cookie")]
+async fn get_cookie(jar: CookieJar) -> Result<impl IntoResponse, Error> {
+    let c = jar.get("foo").ok_or(Error::Message("xx".into()))?;
+    Ok(c.value().to_owned())
 }
