eaebd61950816d2a260f272bd4319d2b7bd13e90
ghtak <gwonhyeong.tak@gmail.com>
2024-12-10 14:12:13 +0000

    tower service trait tutorial

--- hello/src/hello_tower.rs
+++ hello/src/hello_tower.rs
@@ -1,50 +1,39 @@
-use std::future::Future;
 use anyhow::Error;
-use tokio::net::{TcpListener, TcpStream};
+use std::future::Future;
 
-struct Server {
-    address: String,
-}
+struct Server {}
 
 struct Request {}
 
 struct Response {}
 
-async fn read_request(stream: &mut TcpStream) -> Result<Request, Error> {
+async fn read_request() -> Result<Request, Error> {
     Ok(Request {})
 }
 
-async fn write_response(stream: TcpStream, response: Response) {}
+async fn write_response(response: Response) {}
 
 impl Server {
-    fn new(address: &str) -> Self {
-        Server {
-            address: address.into()
-        }
+    fn new() -> Self {
+        Server {}
     }
 
     async fn run<F, Fut>(self, handler: F) -> Result<(), Error>
     where
         F: Fn(Request) -> Fut + Send + Copy + 'static,
-        Fut: Future<Output=Response> + Send,
-    {
-        let listener = TcpListener::bind(&self.address).await?;
-
-        loop {
-            let (mut stream, _) = listener.accept().await?;
-
-            let request = read_request(&mut stream).await?;
-
-            tokio::spawn(async move {
-                let response = handler(request).await;
-                write_response(stream, response).await;
-            });
-        }
+        Fut: Future<Output = Response> + Send,
+    {   
+        let request = read_request().await?;
+        tokio::spawn(async move {
+            let response = handler(request).await;
+            write_response(response).await;
+        });
+        Ok(())
     }
 }
 
-async fn handler_impl(request: Request) -> Response {
-    Response{}
+async fn handle_request(request: Request) -> Response {
+    Response {}
 }
 
 #[cfg(test)]
@@ -58,9 +47,8 @@ mod tests {
             .build()
             .unwrap()
             .block_on(async {
-                let server = Server::new("127.0.0.1:5000");
-                server.run(handler_impl).await?;
-                Ok(())
-            }).expect("TODO: panic message");
+                let server = Server::new();
+                let _result = server.run(handle_request).await;
+            });
     }
 }
